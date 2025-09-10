1763003e4228e02167ebb5975a7d878da0c11d8b
dio-dionis <denkol1775@gmail.com>
2025-09-10 20:55:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print( "hello world!" )
+name = "dionis"
+age = 29
+weight = 80
+is_student = True
+
+print(" name: " + name  + ", " + " age: " + str(age) + ", " + " weight: " + str(weight) + ", " + " is_student: " + str(is_student))
