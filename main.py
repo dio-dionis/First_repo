c384d5beabf5fb94a2aed03d805c4ee6719b6640
dio-dionis <denkol1775@gmail.com>
2025-09-25 22:46:19 +0300

    1st modul 13/15

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-my_list = []
-my_list.insert(0, 2024)
-my_list.insert(1, 'python')
-my_list.insert(2, 3.12)
-
-
+my_list = [2024, 3.12]
+some_data = ['Python']
+my_list.extend(some_data)
+my_list.insert(1,'Python')
+my_list.reverse()
 print(my_list)
