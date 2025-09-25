348d2b355bde18a26c7ab5747e9ebe4caefece64
dio-dionis <denkol1775@gmail.com>
2025-09-25 22:51:32 +0300

    1st modul 14/15

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-my_list = [2024, 3.12]
-some_data = ['Python']
-my_list.extend(some_data)
-my_list.insert(1,'Python')
-my_list.reverse()
-print(my_list)
+data = (year:=2024, lang:='Python', version:=3.12)
+print(data)
