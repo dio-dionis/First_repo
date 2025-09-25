439abd4afbcb7e4282bfd5305e4d7d67a7778f1b
dio-dionis <denkol1775@gmail.com>
2025-09-25 23:05:07 +0300

    1st 15/15

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-data = {}
-data["year"] = 2024
-data["lang"] = "Python"
-data["version"] = 3.12
-print(data)
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+cat["nick"]="simon"
+cat["age"]=7
+cat['characteristics']=["лагідний", "кусається"]
+age = cat.get("age")
+cat.update(info)
+print(cat)
+print(age)
